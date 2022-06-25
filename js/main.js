16aae186ffbc9a9216cfee8b87b40370670f624f
Begum Eryildiz <begumeryildiz@gmail.com>
2022-06-25 16:25:39 +0200

    fixed mouse click

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ class Game {
         this.width = 60;
         this.height = 90;
         this.bubbleRadius = 3;
+        this.velocity = 1;
+        this.velocityX = 0.0;
+        this.velocityY = 0.0;
         
     }
     start(){
@@ -22,34 +25,35 @@ class Game {
             y -= 5;
         }
 
-        let velocityX = 0.5;
-        let velocityY = 0.5;
+        
 
         setInterval(() => {
-            if(this.bubble.positionX + velocityX > this.width - this.bubbleRadius
-                || this.bubble.positionX + velocityX < this.bubbleRadius) {
-                velocityX = -velocityX;
+            if(this.bubble.positionX + this.velocityX > this.width - this.bubbleRadius
+                || this.bubble.positionX + this.velocityX < this.bubbleRadius) {
+                this.velocityX = -this.velocityX;
             }
-            if(this.bubble.positionY + velocityY > this.height - this.bubbleRadius
-                || this.bubble.positionY + velocityY < this.bubbleRadius) {
-                velocityY = -velocityY;
+            if(this.bubble.positionY + this.velocityY > this.height - this.bubbleRadius
+                || this.bubble.positionY + this.velocityY < this.bubbleRadius) {
+                this.velocityY = -this.velocityY;
             }
-            this.bubble.moveBy(velocityX,velocityY);
+            this.bubble.moveBy(this.velocityX, this.velocityY);
         }, 50);
     }
-
-    // function printMousePos(event) {
-    //     document.body.textContent =
-    //       "clientX: " + event.clientX +
-    //       " - clientY: " + event.clientY;
-    //   }
-      
-    //   document.addEventListener("click", printMousePos);
     
     attachEventListeners(){
         document.addEventListener('click', (event) => {
             console.log( "clientX: " + event.clientX +
             " - clientY: " + event.clientY);
+
+        const velocityPixelX = event.clientX - this.bubble.getCenterX();
+        const velocityPixelY = -(event.clientY - this.bubble.getCenterY());
+        console.log("getcenterX " + this.bubble.getCenterX())
+        console.log("getcenterY " + this.bubble.getCenterY())
+        const diagonalPixel = Math.sqrt(velocityPixelX ** 2 + velocityPixelY ** 2);
+        const moveRatio = this.velocity / diagonalPixel;
+        this.velocityX = velocityPixelX * moveRatio;
+        this.velocityY = velocityPixelY * moveRatio;
+
         });
     }
 }
@@ -81,6 +85,19 @@ class Bubble {
 
         const boardElm = document.getElementById("board");
         boardElm.appendChild(this.domElement);
+    }
+
+    getCenterX() {
+        let viewportOffset = this.domElement.getBoundingClientRect();
+        let centerX = viewportOffset.left + viewportOffset.width / 2;
+        return centerX;
+        
+    }
+
+    getCenterY() {
+        let viewportOffset = this.domElement.getBoundingClientRect();
+        let centerY = viewportOffset.top + viewportOffset.height / 2;
+        return centerY;
     }
 
     moveBy(velocityX, velocityY){
