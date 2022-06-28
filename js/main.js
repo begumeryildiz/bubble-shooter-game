6193b523fe58d15bc45ff3e28bd40f9d5635ffaf
Begum Eryildiz <begumeryildiz@gmail.com>
2022-06-28 21:33:19 +0200

    audio fix

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ class Game {
         this.velocityY = 0;
         this.score = 0;
         this.yippieSound = new Audio('../resources/audios/yippee-sound.mp3');
-        this.gameBackgroundSound = new Audio('../resources/audios/game-background-audio.mp3');
+        this.gameBackgroundSound = document.getElementById('background-audio');
         this.gameBackgroundSound.loop = true;
         this.gameOverSound = new Audio('../resources/audios/gameover-audio.mp3');
         this.minClusterSize = 3;
